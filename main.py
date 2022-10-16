a569b1aa04b0d1822b96beffe1c06fa8b136575c
TheAnister <47427741+TheAnister@users.noreply.github.com>
2022-10-16 13:05:13 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,49 @@
+## Defaults
+short = False
 
+## Functions
+
+def webpageViewer():
+    try:
+        import os
+        import webbrowser
+        import sys
+        from time import sleep
+        from bs4 import BeautifulSoup
+        import requests
+        import tkinter as tk
+        from tkinter import simpledialog
+        import getpass
+        import asyncio
+    except:
+        print("Error occured. Cannot access webpage-viewer because following modules need to be installed (via 'pip install (modulename)'): os, webbrowser, sys, time, bs4, requests, tkinter, getpass (used to identify username to write files to specific user), asyncio")
+        
+    print("Successfully imported modules. \n")
+    print("Welcome to JOS's WEBVIEWER - A simple tool used to browse or search the internet!")
+    print("Would you like to search (search) or open up a website (website)?")
+    query = input("> ")
+    if query == "search":
+        print("Excellent! What would you like to search for?")
+        search = str(input("> "))
+        print("What search engine? Please choose from: Google, Bing, Yahoo!, YouTube, Wikipedia, Dictionary")
+        engine = input("> ")
+        if engine == "google" or "Google":
+            webbrowser.open("https://www.google.com/search?q=" + search)
+        elif engine == "bing" or "Bing":
+            webbrowser.open("https://www.bing.com/search?q=" + search)
+        elif engine == "yahoo" or "Yahoo" or "Yahoo!" or "yahoo!":
+            webbrowser.open("https://www.search.yahoo.com/search?p=" + search)
+        elif engine == "youtube" or "Youtube" or "YouTube" or "you tube":
+            webbrowser.open("https://www.youtube.com/results?search_query=" + search)
+        elif engine == "wikipedia" or "Wikipedia":
+            webbrowser.open("https://en.wikipedia.org/wiki/" + search)
+        elif engine == "Dictionary" or "dictionary":
+            webbrowser.open("https://www.oxfordlearnersdictionaries.com/definition/english/" + search)
+    elif query == "website":
+        print("Excellent! What website would you like to visit?")
+        website = input("> ")
+        webbrowser.open(str(website))
+        
 
 ## modules
 import os
@@ -37,6 +82,31 @@ except:
     sleep(5)
     os.system('python -m pip install colorama termcolor')
     exit()
+try:
+    import datetime
+except:
+    print("Error installing datetime. Fixing issues... (Fix 1/2)")
+    try:
+        os.system('python -m pip install datetime')
+    except:
+        ## hacker.!!!.
+        print("Error installing module (module already there / corrupt?). Fixing issues... (Fix 2/2)")
+        sleep(5)
+        print(Fore.RED + "Error occured! This was not meant to happen! You have corrupted the system! Automatically exiting..." + Fore.RESET)
+        try:
+            exit()
+        except:
+            print(Fore.RED + "ERROR OCCURED! CANNOT EXIT!")
+            while True:
+                print("Error Cannot exit! Timing process out, please wait!")
+            while True:
+                print("Error Cannot exit! Timing process out, please wait!")
+            while True:
+                print("Error Cannot exit! Timing process out, please wait!")
+            while True:
+                print("Error Cannot exit! Timing process out, please wait!")
+            while True:
+                print("Error Cannot exit! Timing process out, please wait!")
 os.system("cls" or "clear")
 print("\n\n\n\n\n\n\n\n\n")
 ## functions
@@ -61,14 +131,42 @@ try:
         firstuser = "true"
 except:
     print(Fore.RED + "Cannot access C:\ Drive. Please ensure that you have access to it in order to use commands such as "+ Fore.BLUE + "get" + Fore.RED + "." + Fore.RESET)
+
+## Password login/creator
+
 try:    
     if firstuser == "true":
-        print(Fore.CYAN + "Welcome to JaskaranOS! This is a command-based operating system. For help on commands, please type in: 'help' in the command line!\nThis OS has been tested on a Windows Machine. It currently does not have the capablities to be run on it's own, separate computer." + Fore.RESET)
+        print(Fore.CYAN + "Welcome to JaskaranOS! This is a command-based operating system. For help on commands, please type in: 'help' in the command line!\nThis OS has been tested on a Windows Machine. It will soon be able to be run on it's own computer" + Fore.RESET)
+        print("\n Please set a password below!")
+        password = input("> ")
+        f = open("C://JaskaranOS/password.txt", "w")
+        f.write(str(password))
+        f.close
+        print("Successfully set password!")
+    else:
+        print("\nPlease enter your password!")
+
+        f = open("C://JaskaranOS/password.txt", "r")
+        urpass = str(f.read())
+        x = "0"
+        while x == "0":
+            password = input("> ")
+            if str(password) == urpass:
+                print("Succesfully authorised user.")
+                x = "1"
+            else:
+                print("Error: Invalid password.")
+        
+        f.close()
+
 except:
-    print("")
+    print("") ## C:// unaccessible or not working, can't store data
+
+print("Type 'help' for commands!\n\n")
+
 # INPUT
 while True:
-    a = input("> ")
+    a = input("sudo@root > ")
 
 # now the processing!
 
@@ -76,13 +174,15 @@ while True:
     if a == "help":
         print(Back.BLUE + "Welcome to JaskaranOS help! Here are the available commands:\nhelp - Opens this\necho (string) - Echoes (repeats) back\nls (string) - List Files in Directory\ntext (string/directory) - Creates a new text file\nwrite (string) - Write to a text file. First write what you want, then it will ask you for the directory. Use \ n (without spaces) for a new line.")
         print("read (directory) - Reads text file specified by user.")
-        print("get (url) - [BETA] Download files over the internet. Requires WGET module.")
         print("run (directory) - Runs an application.")
         print("ping (website) - Pings a website to check if it can connect.\nsay (input) - Text To Speech engine. Change voice by saying 'say male' or 'say female'")
         print("username / user - Checks your username. Requires GETPASS module.")
         print("password / pass - Asks for password to continue. Doesn't work on some compilers.")
         print("cmd (input) - Used windows CMD to run advanced commands.")
-        print("pip install (input) - Installs python modules using PIP." + Back.RESET)
+        print("pip install (input) - Installs python modules using PIP.")
+        print("download (url) - Downloads and installs a URL.")
+        print("password (new pass) - Change your password")
+        print("current pass - View your current password")
 
  # Echo
     elif a[0:5] == "echo ":
@@ -164,17 +264,24 @@ while True:
     elif a == "hi" or a == "hello" or a == "hey there" or a == "what do you do" or a == "what do you do?":
         print(Fore.GREEN + "Hello, there! Please type in 'help' for help on commands!" + Fore.RESET)
         
-    elif a[0:7] == "colour " or a[0:6] == "color ":
+    elif a[0:7] == "colour " or a[0:6] == "color " or a == "color " or a == "colour ":
         if a[0:7] == "colour ":
             color = a[7:]
+        elif a == "color " or a == "colour ":
+            print("Please specify a "+Fore.GREEN+"colour."+Fore.RESET)
+            color = "(from colorama import colourname)\nERROR OCCURED: CANNOT IMPORT COLOR ERROR FROM COLORAMA"
         else:
             color = a[6:]
         print("Colour is now", color)
         try:
             print(Fore.color + "Testing to check colour..")
         except:
-            print("\nError: Color system is currently " + Fore.RED + "offline. " + Fore.RESET + "Please try again later.")
-            
+            print("\nError: Color system is currently " + Fore.RED + "offline. " + Fore.RESET + "Please try again later. For more information, type:\n'"+Fore.BLUE+"color-offline"+Fore.RESET+"'")
+    
+    elif a == "color-offline" or a == "colour-offline":
+        print("The colour system is currently"+Fore.RED+" offline"+Fore.RESET+". This is because the "+Fore.GREEN+"colour"+Fore.BLUE+" system "+Fore.RESET+"is currently undergoing heavy development. Please check back in future updates.")
+        
+        
     elif a[0:4] == "say ":
         say = str(a[4:])
         try:
@@ -230,8 +337,57 @@ while True:
         pip = a[12:]
         os.system("python -m pip install "+pip)
     
+    elif a == "web":
+        webpageViewer()
+        
+    elif a[0:8] == "install " or a[0:9] == "download ":
+        if a[0:8] == "install ":
+            asd = a[8:]
+        elif a == "install " or a == "download ":
+            print(Fore.RED+"Invalid. Please specifiy a download link.")
+        else:
+            asd = a[9:]
+        
+        if not asd[0:4] == "http":
+            print(Fore.RED + "Error. Cannot get webpage "+Fore.BLUE+asd+Fore.RESET+Fore.RED+". Please make sure URL begins with: http"+Fore.RESET)
+        else:
+            response = requests.get(asd)
+            print("File name? Please include extention.")
+            fname = input("> ")
+            open("C://JaskaranOS/"+fname, "wb").write(response.content)
+            print("Successfully downloaded URL "+asd+" to: C://JaskaranOS/"+fname+". Would you like to open the folder?")
+            yesorno = input("> ")
+            if yesorno == "yes":
+                print("Opening Folder... please stand by.")
+                os.startfile(os.path.realpath("C://JaskaranOS"))
     
+    elif a == "wwazzup" or a == "whats up" or a == "what's up" or a == "yo":
+        print("Hi.")
+        
+    elif a == "lol" or a == "xd" or a == "xD" or a == "XD":
+        print("What's so funny?")
+        
+    elif a == "current pass":
+        f = open("C://JaskaranOS/password.txt", "r")
+        print("Your current password is "+f.read())
+        f.close()
+        
+    elif a[0:9] == "password ":
+        newpass = a[9:]
+        print("You are about to change your password to '"+newpass+"'! Please input your current password to confirm.")
+        currentpass = input("> ")
+        f = open("C://JaskaranOS/password.txt", "r");
+        if str(currentpass) == (f.read()):
+            f = open("C://JaskaranOS/password.txt", "w")
+            f.write(str(newpass))
+            f.close
+            print("Successfully changed password.\n")
+        else:
+            print(Fore.RED+"Invalid password."+Fore.RESET)
+        f.close()
+        
     
     else:
         print(Fore.RED + "Unknown command. Please type in 'unknown command' for more information." + Fore.RESET)
+        
     
